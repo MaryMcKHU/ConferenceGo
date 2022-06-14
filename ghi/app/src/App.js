183a21f4aa6a77e88a91abdbb1a4bf1e8cce6f0d
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-06-14 13:59:23 -0700

    Added mainpage and changed nav and app accordingly

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -6,21 +6,37 @@ import React from 'react';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
 import AttendConferenceForm from './AttendConferenceForm';
+import PresentationForm from './PresentationForm';
+import MainPage from './MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <>
+    <BrowserRouter>
     <Nav />
-    <div className="container">
-      < AttendConferenceForm />
-      {/* <ConferenceForm /> */}
-      {/* <LocationForm /> */}
-      {/* <AttendeesList attendees={props.attendees} /> */}
-    </div>
-    </>
+      <Routes>
+        <Route path="home" element={<MainPage />} />
+          <Route index element={<MainPage />} />
+        <Route path="conferences">
+          <Route path="new" element={<ConferenceForm />} />
+        </Route>
+        <Route path="attendees">
+          <Route path="new" element={<AttendConferenceForm />} />
+        </Route>
+        <Route path="locations">
+          <Route path="new" element={<LocationForm />} />
+        </Route>
+        <Route path="presentations">
+          <Route path="new" element={<PresentationForm />} />
+        </Route>
+        <Route path="attendees" element={<AttendeesList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
